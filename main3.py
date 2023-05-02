ac9f3c4b9453996478afc6850356b9cd249b2fb6
Abdullah171 <aqibawan926@gmail.com>
2023-05-02 13:48:45 +0500

    done with the hex pdf chatbot api and local redis

---
+++ main3.py
@@ -0,0 +1,85 @@
+import json
+import redis
+from datetime import timedelta
+from fastapi import FastAPI
+import os
+import openai
+from langchain.document_loaders import PDFMinerLoader
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+app = FastAPI()
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
+
+def load_docs():
+    loader = PDFMinerLoader("hexdata.pdf")
+    data = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1400, chunk_overlap=400)
+    texts = text_splitter.split_documents(data)
+    faiss_index = FAISS.from_documents(texts, OpenAIEmbeddings())
+    return faiss_index
+
+class Chatbot:
+    def __init__(self):
+        self.faiss_index = load_docs()
+
+def load_hex():
+    with open("hexcoin.txt", "r") as file:
+        data = file.read()
+    return data
+
+chatbot = Chatbot()
+
+def get_redis_key():
+    return "hex_data"
+
+def set_redis_data(data):
+    key = get_redis_key()
+    redis_client.set(key, json.dumps(data))
+    redis_client.expire(key, timedelta(days=1))
+
+def get_redis_data():
+    key = get_redis_key()
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None
+
+@app.get("/")
+async def root():
+    return "crypto-api"
+
+@app.post("/start_chat")
+async def start_chat(user_input):
+
+    redis_data = get_redis_data()
+    if redis_data:
+        hex_data = redis_data["docs"]
+    else:
+        hex_data = load_hex()
+        set_redis_data({"docs": hex_data})
+
+    docs=chatbot.faiss_index.similarity_search(user_input, k=2)
+
+    messages =[
+            {"role": "system", "content":f"""You are a chatbot that is restricted to hex currency and can answer questions only related to Hex Crypto currency. You will refuse to answer any question that is not related to hex. If the conversation is not related to hex coin try to bring back 
+            conversation to hex related only. I am going to provide you a few documents that contains information about hex and also some FAQ. If the user asks any question or information that is present or 
+            related to the information in the provided documents then answer to that question, provide information or generate an answer to user using only these documents, don't answer from your own. If the user asks some question that is not present in the documents then 
+            answer using your information.
+            Provided Documents Start: {docs}. Provided Documents End."""},
+        ]
+    messages.append({"role": "user", "content": user_input})
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+    )
+
+    message_content = response['choices'][0]['message']['content']
+    role = response['choices'][0]['message']['role']
+
+    response_dict = {"role": role, "content": message_content}
+    return response_dict
