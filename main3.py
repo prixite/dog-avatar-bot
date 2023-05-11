bfa19fa1d2563f798e944410d9cd66cd3375b776
Abdullah171 <aqibawan926@gmail.com>
2023-05-11 12:45:42 +0500

    added the docker files

--- main3.py
+++ main3.py
@@ -12,7 +12,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 os.environ["OPENAI_API_KEY"] = "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"
 
 app = FastAPI()
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
+# redis_client = redis.Redis(host="localhost", port=6379, db=0)
+redis_client = redis.Redis(host="redis", port=6379, db=0)
+
 
 def load_docs():
     loader = PDFMinerLoader("hexdata.pdf")
